183ebc9d31f457ed08ac3ff012a7c3a2da44bfaa
David Brown <dibidave@gmail.com>
2016-01-25 17:28:51 -0800

    Initial commit

---
+++ bin/trimlines.py
@@ -0,0 +1,15 @@
+import sys
+
+input_file = open(sys.argv[1], 'r')
+output_file = open(sys.argv[2], 'w')
+
+line_counter = 0
+
+for line in input_file:
+	if line_counter % 4 == 1:
+		output_file.write('>Sequence' + str((line_counter + 3)/4) + '\n')
+		output_file.write(line)
+	line_counter += 1
+
+input_file.close()
+output_file.close()
