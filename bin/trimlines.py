da6237d7162bbc68e0478dff3a732ae22f96047f
David Brown <dibidave@gmail.com>
2016-02-15 14:22:43 -0800

    Add quality scores to variant sequences file

--- bin/trimlines.py
+++ bin/trimlines.py
@@ -1,15 +1,22 @@
 import sys
 
 input_file = open(sys.argv[1], 'r')
-output_file = open(sys.argv[2], 'w')
+fasta_file = open(sys.argv[2], 'w')
+quality_file = open(sys.argv[3], 'w')
 
 line_counter = 0
+sequence_count = 0
 
 for line in input_file:
 	if line_counter % 4 == 1:
-		output_file.write('>Sequence' + str(int((line_counter + 3)/4)) + '\n')
-		output_file.write(line)
+		sequence_count += 1
+		fasta_file.write('>Sequence' + str(sequence_count) + '\n')
+		fasta_file.write(line)
+	elif line_counter % 4 == 3:
+		quality_file.write('>Sequence' + str(sequence_count_ + '\n'))
+		quality_file.write(line)
 	line_counter += 1
 
 input_file.close()
-output_file.close()
+fasta_file.close()
+quality_file.close()
