59567e0edb135fe30923ceb18bc624c7cd551eb5
David Brown <dibidave@gmail.com>
2016-05-21 02:47:51 -0700

    Abstracting out ML analysis functions

--- utils.py
+++ utils.py
@@ -7,3 +7,14 @@ def convert_string_to_char_array(string):
 		char_array.append(char);
 
 	return char_array
+
+def convert_sequence_label_dict_to_matrices(sequence_label_dict):
+
+	sequence_matrix = []
+	label_matrix = []
+
+	for sequence, feature in sequence_label_dict:
+		sequence_matrix.append(convert_string_to_char_array(sequence))
+		label_matrix.append([feature])
+
+	return sequence_matrix, label_matrix
