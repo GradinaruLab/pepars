c0f8566c6a679ee0868b52c0e742d83c55e5b8e4
David Brown <dibidave@gmail.com>
2016-07-13 21:36:42 -0700

    Deleted duplicate perfect_match_aligner

--- alignment/Aligner.py
+++ alignment/Aligner.py
@@ -1,10 +1,4 @@
 class Aligner():
 
     def __init__(self):
-        self.something = ''
-
-class Perfect_Match_Aligner(Aligner):
-
-    def __init__(self):
-    	Aligner.__init__(self)
-        self.method = 'Perfect_Match'
+        pass
