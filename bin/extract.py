77bc0a3733147532a68b0c7d7d0f2c5541ef894c
Petur Helgi Einarsson <phe1@hi.is>
2016-08-08 21:20:43 -0700

    SOme changes to alignment statistics tab

--- bin/extract.py
+++ bin/extract.py
@@ -3,24 +3,49 @@ import re
 import time
 from os import listdir
 from os.path import isfile, join
+import subprocess
+
 codons = [3, 3, 3, 3]
 num_insertions = len(codons)
-# allowed = ['G', 'T', 'A', 'C']
-allowed = ['G', 'T', 'A']
-allowed.append('N')
+allowed = ['G', 'T']
+
+limit = 0
+qlt_limit = 0.5
+
+print subprocess.call('pwd')
+names = {}
+end = '_bowtie_alignment_CIGAR_sequences.txt'
+my_path = '/home/peturhelgi/documents/code/virusfarm/Data/TestData/'
+# files = [file for file in listdir(my_path) if isfile(join(my_path,file)) and end in file]
+#files = ['17124_bowtie_alignment_CIGAR_sequences.txt']
+
+##########################
+# files=['17124_bowtie_alignment_CIGAR_sequences.txt']
+# files=['example_17124_cigar.txt']
+files=['quality_test.txt']
+
+
 '''
  1456  bowtie2-build reference.fa YichengReference
 
- 1459  bowtie2 --local -x YichengReference -U 17115_ATCACG_L001_R1_001.fastq -S 17115_alignment.sam
- 1462  cut -f1,6,10 17115_alignment.sam > 17115_bowtie_alignment_CIGAR_sequences.txt
+ 1459  bowtie2 --local -x YichengReference -U 17124_AGTTCC_L001_R1_001.fastq -S 17124_alignment0804.sam
+ 1462  cut -f1,6,10 17124_alignment0804.sam > 17124_cigar0804.txt
 '''
-end = '_bowtie_alignment_CIGAR_sequences.txt'
-my_path = '../Data/TestData/'
+
 
 def parse(id, cigar, seq):
     index = 0
     chars = re.findall('[^\d]+',cigar)
     nums = re.findall('\d+',cigar)
+    index_of_var = 0
+    indices = []
+#####################################################
+
+    # print chars
+    # print nums
+
+#####################################################
+
 
     row = []
     for i, (char, num) in enumerate(zip(chars, nums)):
@@ -29,43 +54,64 @@ def parse(id, cigar, seq):
             break
         if char=='I' and num == 3:
             codon = seq[index:index+num]
+            index_of_var +=1
             if codon[-1] not in allowed:
                 continue
+                # pass
+            indices.append(index_of_var)
             row.append(codon)
         if char != 'D':
             index += num
 
+######################################################
+
+    # print row, indices
+
+######################################################
+
     if len(row) != num_insertions:
         return
+    # if chars == ['S', 'M', 'I', 'M', 'I', 'M', 'I', 'M', 'I', 'M', 'I', 'M', 'S']:
+    #     print seq
     variants.append(row)
     seqs.append(seq)
     ids.append(id)
-# files = [file for file in listdir(my_path) if isfile(join(my_path,file)) and end in file]
-files = ['17124_bowtie_alignment_CIGAR_sequences.txt']
-names = {}
+
+count = 0
 for file in files:
+    print my_path,file
     ids = []
     variants = []
     seqs = []
     name = file
     start = time.clock()
-    file = open(join(my_path,file), 'r').readlines()[3:]
-
+    if limit != 0:
+        file = open(join(my_path,file), 'r').readlines()[3:limit+3]
+    else:
+        print 'limit'
+        file = open(join(my_path,file), 'r').readlines()[3:]
+        limit = len(file)
+        
     for line in file:
         line = line.split('\t')
         id = line[0]
         cigar = line[1]
         seq = line[2]
-        parse(id, cigar, seq)
+        qlt = line[3]
+        if float(len(re.findall('\w', qlt)))/float(len(qlt)) < qlt_limit:
+            count += 1
+            continue
+        if cigar!='*':
+            parse(id, cigar, seq)
     end = time.clock()
     print name, 'took', end-start,'seconds'
 
     if not (len(ids) == len(seqs) == len(variants)):
         print 'Not consistent'
-    print 'File had', len(variants), '/', len(file),'matches'
-    acc = (0.0+len(variants))/(0.0+len(file))
+    print 'File had', len(variants), '/', len(file)-count,'matches'
+    acc = (0.0+len(variants))/(0.0+len(file)-count)
     print 'Accuracy:', acc,'\n'
-
+    print 'Number of seqs deleted due to low quality:',count,'out of' ,limit, '. That is',str(float(count)/float(limit))+'%'
     names[name]={'Total':len(file), 'Matched':len(ids), 'Accuracy':acc}
 
 for name in sorted(names):
