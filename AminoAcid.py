a623bf2beae81a276fc14f058a48c735df38a425
Tatyana Dobreva <cosmotat@gmail.com>
2016-10-07 23:38:25 -0700

    added more features for aa

--- AminoAcid.py
+++ AminoAcid.py
@@ -12,7 +12,7 @@ def compare_to(val1, val2):
 
 class AminoAcid:
 
-	properties = ['charge','hydropathy','solubility','phosphorylation','average_flexibility_idx','ionic_bond','molecular_weight','hydrophobicity',]
+	properties = ['charge','hydropathy','solubility','phosphorylation','average_flexibility_idx','ionic_bond','molecular_weight','hydrophobicity','typically_helix','typically_turn','typically_sheet']
 
 	
 	def __init__(self,amino_acid):
