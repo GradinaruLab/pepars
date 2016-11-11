9cb421bff1c8cbd9528ce8cbe602d64af00bfcdd
David Brown <dibidave@gmail.com>
2016-11-10 20:33:47 -0800

    Calculate sequence coverage by theoretical IUPAC possibilities without storing them in memory

--- analysis/coverage.py
+++ analysis/coverage.py
@@ -1,5 +1,7 @@
 from utils import DNA
 import itertools
+from workspace import Workspace as ws
+from workspace import Database as db
 
 def get_possible_sequences_by_template(template, by_amino_acid = True):
 
@@ -107,7 +109,7 @@ def get_possible_sequences(analysis_set, by_amino_acid = True, use_multiple_temp
             sequences.add(sequence)
     return sequences
 
-def get_coverage(analysis_set, by_amino_acid = True, use_multiple_templates = False):
+def get_coverage_sequences(analysis_set, by_amino_acid = True, use_multiple_templates = False):
 
     possible_sequences = get_possible_sequences(analysis_set, by_amino_acid, use_multiple_templates)
 
@@ -133,3 +135,48 @@ def get_coverage(analysis_set, by_amino_acid = True, use_multiple_templates = Fa
             excluded_sequences.add(possible_sequence)
 
     return included_sequences, excluded_sequences
+
+def get_coverage(analysis_set, by_amino_acid = True):
+
+    num_included_sequences = 0
+
+    variant_template = ''
+
+    alignment = ws.get_active_alignment()
+
+    # Get the templates for each analysis set
+    for library_name in analysis_set.get_libraries():
+        db_library = db.get_library(library_name)
+        template_id = alignment.library_templates[db_library.id]
+        template = db.get_template_by_id(template_id)
+        if len(variant_template) == 0:
+            variant_template = template.get_variant_template()
+        elif variant_template != template.get_variant_template():
+            raise Exception('Variant templates must match in an analysis set to do coverage analysis!')
+
+    num_possible_sequences = 1
+
+    if by_amino_acid:
+        if len(variant_template) % 3 != 0:
+            raise Exception('Can\'t analyze by amino acid when variant sequence isn\'t groups of 3!')
+        variant_length = len(variant_template) / 3
+
+        # For now, assume all amino acids are possible. Should do something with IUPAC later
+        for variant_index in range(0, variant_length):
+            num_possible_sequences *= 20;
+    else:
+        for variant_index in range(0, len(variant_template)):
+            num_possible_sequences *= len(DNA.IUPAC[variant_template[variant_index]])
+
+    # We assume all sequences in the analysis set have been aligned against the template,
+    # so they must match the template. So, all unique sequences are the included sequences
+
+    included_sequences = set()
+
+    for library_name, library in analysis_set.get_libraries().items():
+        sequence_counts = library.get_sequence_counts(by_amino_acid, count_threshold = 0)
+
+        for sequence, count in sequence_counts.items():
+            included_sequences.add(sequence)
+
+    return len(included_sequences), num_possible_sequences
