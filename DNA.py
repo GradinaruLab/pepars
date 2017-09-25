3d4b49103d723efb843686f0a4432d79b09e7b35
David Brown <dibidave@gmail.com>
2017-09-25 13:38:24 -0700

    Added get sequence distance function

--- DNA.py
+++ DNA.py
@@ -69,3 +69,18 @@ def get_reverse_complement(sequence):
 	reverse_complement_sequence = get_complement(reverse_sequence)
 
 	return reverse_complement_sequence
+
+def get_sequence_distance(sequence_1, sequence_2):
+
+	min_length = min(len(sequence_1), len(sequence_2))
+	max_length = max(len(sequence_1), len(sequence_2))
+
+	distance = 0
+
+	for character_index in range(0, min_length):
+		if sequence_1[character_index] != sequence_2[character_index]:
+			distance += 1
+
+	distance += max_length - min_length
+
+	return distance
