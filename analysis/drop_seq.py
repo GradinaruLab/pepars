0bba1b8d45ccfd44ff3ca88de292ddfcce45f4f9
David Brown <dibidave@gmail.com>
2017-09-26 14:02:48 -0700

    Cleaning up some comments

--- analysis/drop_seq.py
+++ analysis/drop_seq.py
@@ -28,27 +28,4 @@ def get_barcodes_from_transcript_matrix(file_path):
         barcodes.append(barcode)
         barcode_counts.append(0)
 
-    print(data_frame.sum())
-
-
-    # for index, row in data_frame.iterrows():
-
-    #     print(index)
-
-    #     if index == 0:
-    #         continue
-
-    #     for cell_index in range(0, num_cells):
-
-    #         count = row[cell_index + 1]
-
-    #         barcode_counts[cell_index] += count
-
-    # for barcode_index in range(0, len(barcodes)):
-
-    #     if barcode_counts[barcode_index] == 0:
-    #         num_zero_counts += 1
-
-    # print("Num zero counts: %i" % num_zero_counts)
-
     return barcodes
