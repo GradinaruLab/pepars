20907cae5408d72d0aaab3a5d7ae0296d03e6e0d
David Brown <dibidave@gmail.com>
2017-10-03 16:39:22 -0700

    Header is now optional in CSV wrapper

--- fileio/csv_wrapper.py
+++ fileio/csv_wrapper.py
@@ -5,7 +5,8 @@ def write_csv_file(file_name, header, rows):
 
     file = open(file_name, 'w')
     writer = csv.writer(file)
-    writer.writerow(header)
+    if header != None:
+        writer.writerow(header)
 
     for row in rows:
         writer.writerow(row)
